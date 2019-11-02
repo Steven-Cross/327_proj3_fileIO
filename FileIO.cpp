1dead16daa07b09ca39fb0eeedaf22e4a51ff36d
Steven Cross <steven.cross.17@cnu.edu>
2019-11-02 07:03:00 -0700

    Wrote fileIO.cpp

--- FileIO.cpp
+++ FileIO.cpp
@@ -1,24 +1,57 @@
 /*
  * FileReader.cpp
  *
- *  Created on: Oct 8, 2017
- *      Author: keith
+ *  Edited on: November 2nd, 2019
+ *      Author: Steven Cross
  */
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace std;
 
+	//trys to open filename, and return its contents in contents
+	//it will strip out only \n's (char return\line feed)
+	//returns:
+	//SUCCESS all worked, contents string contains all in the file
+	//COULD_NOT_OPEN_FILE_TO_READ contents string is cleared
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	ifstream myFile;
+	myFile.open(filename);
+	contents = "";
+	string line;
+
+	if (!myFile.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+	while (!myFile.eof()){
+		getline(myFile, line, '\n');
+		contents.append(line);
+	}
+	return SUCCESS;
 }
 
+//trys to open filename, and serialize  myEntryVector contents to it
+	//returns:
+	//SUCCESS all worked, filename contains myEntryVector strings
+	//COULD_NOT_OPEN_FILE_TO_WRITE
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	ofstream myFile;
+	myFile.open(filename);
+
+	if (!myFile.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+
+	for (int i = 0; i < myEntryVector.size(); i++) {
+		myFile << myEntryVector[i] << endl;
+	}
+
+	return SUCCESS;
 }
 
 
